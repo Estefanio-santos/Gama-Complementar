10239100304a1a405454978d9ff6656386d2623f
Estefanio-santos <mr.estefanio@hotmail.com>
2022-03-15 11:03:27 -0300

    primeiro exercicio

---
+++ Aula01/Exercicio1/src/ProgramaMedia.java
@@ -0,0 +1,14 @@
+
+public class ProgramaMedia {
+
+public static void main(String[] args) {
+
+double nota1, nota2;
+nota1 = 8.5;
+nota2 = 9.5;
+double media = ( nota1 + nota2 ) / 2;
+System.out.println("A média entre as notas 1 e 2 é de: " + media);
+
+}
+
+}
